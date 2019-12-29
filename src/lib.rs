9692de67b54da6737fc7d8255a2f7a639d969d8f
cwouyang <cwouyang@protonmail.com>
2019-12-29 16:32:54 +0800

    feat(log): add pretty_env_logger 0.4.8 as logging backend

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,21 @@
 extern crate chrono;
-extern crate strum;
-#[macro_use]
-extern crate strum_macros;
 #[macro_use]
 extern crate lazy_static;
 #[cfg(test)]
 #[macro_use]
 extern crate load_file;
+#[macro_use]
+extern crate log;
+extern crate pretty_env_logger;
 extern crate regex;
 extern crate reqwest;
 extern crate select;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
+extern crate strum;
+#[macro_use]
+extern crate strum_macros;
 extern crate url;
 
 pub mod article;
